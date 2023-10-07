2d599c109f4c66957f864cafcfa6b0860dae4b9b
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-10-07 13:30:04 +0200

    working on top teachers component

---
+++ front/src/components/Teachers.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container mx-auto mb-14 p-8">
+        <h1 class="text-2xl text-text font-semibold mb-4 text-center">Learn from Creative Experts</h1>
+        <p class="text-center text-text pb-8 pt-3">Skillshare classes are taught by industry leaders excited to share their tools, techniques, and professional journeys with you.</p>
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+            <!-- Teacher 1 -->
+            <div class="bg-white relative border rounded shadow-md overflow-hidden">
+                <img src="https://via.placeholder.com/150" alt="Teacher 1" class="w-full h-auto">
+                <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100">
+                    <h2 class="text-lg font-semibold text-white mb-2">Teacher 1</h2>
+                    <p class="text-white">Subject: Math</p>
+                </div>
+            </div>
+            
+            <!-- Teacher 2 -->
+            <div class="bg-white relative border rounded shadow-md overflow-hidden">
+                <img src="https://via.placeholder.com/150" alt="Teacher 2" class="w-full h-auto">
+                <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100">
+                    <h2 class="text-lg font-semibold text-white mb-2">Teacher 2</h2>
+                    <p class="text-white">Subject: Science</p>
+                </div>
+            </div>
+
+            <!-- Add similar code for Teacher 3 to Teacher 6 -->
+            <div class="bg-white relative border rounded shadow-md overflow-hidden">
+                <img src="https://via.placeholder.com/150" alt="Teacher 2" class="w-full h-auto">
+                <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100">
+                    <h2 class="text-lg font-semibold text-white mb-2">Teacher 2</h2>
+                    <p class="text-white">Subject: Science</p>
+                </div>
+            </div>
+            <div class="bg-white relative border rounded shadow-md overflow-hidden">
+                <img src="https://via.placeholder.com/150" alt="Teacher 2" class="w-full h-auto">
+                <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100">
+                    <h2 class="text-lg font-semibold text-white mb-2">Teacher 2</h2>
+                    <p class="text-white">Subject: Science</p>
+                </div>
+            </div>
+            <div class="bg-white relative border rounded shadow-md overflow-hidden">
+                <img src="https://via.placeholder.com/150" alt="Teacher 2" class="w-full h-auto">
+                <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100">
+                    <h2 class="text-lg font-semibold text-white mb-2">Teacher 2</h2>
+                    <p class="text-white">Subject: Science</p>
+                </div>
+            </div>
+            <div class="bg-white relative border rounded shadow-md overflow-hidden">
+                <img src="https://via.placeholder.com/150" alt="Teacher 2" class="w-full h-auto">
+                <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 transition-opacity opacity-0 hover:opacity-100">
+                    <h2 class="text-lg font-semibold text-white mb-2">Teacher 2</h2>
+                    <p class="text-white">Subject: Science</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
