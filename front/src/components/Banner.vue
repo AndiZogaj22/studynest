a9d3e846bfa2dcae971e24a78574f45d03706b4e
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-10-07 13:58:31 +0200

    teacher component and some responsive work

--- front/src/components/Banner.vue
+++ front/src/components/Banner.vue
@@ -1,39 +1,50 @@
 <template>
-    <div >
-      <div class="max-w-screen-lg mx-auto  rounded-lg flex">
-          <div class="w-1/2 mb-auto">
-              <h1 class="text-7xl font-semibold">Get Creative<br> With <br><div class="hover:text-primary">StudyNest</div></h1>
+    <div class="bg-ingerit ">
+      <div class="container mx-auto p-4">
+        <div class="md:flex md:max-w-screen-lg mx-auto rounded-lg">
+          <div class="md:w-1/2 mb-8 md:mb-0">
+            <h1 class="text-5xl md:text-7xl font-semibold">
+              Get Creative<br> With <br>
+              <span class="hover:text-primary">StudyNest</span>
+            </h1>
           </div>
-          <div class="w-1/2 p-2 mb-16">
-    <ul class="list-none pl-4 text-2xl">
-        <li class="whitespace-nowrap mb-10 flex items-center">
-            <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-         </svg>
-            Learn creative skills to achieve your personal and professional goals.
-        </li>
-        <li class="whitespace-nowrap mb-10 flex items-center">
-            <svg class="w-4.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-         </svg>
-            Tune in and level up at your own pace.
-        </li>
-        <li class="whitespace-nowrap mb-10 flex items-center">
-            <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-         </svg>
-            Go from dabbler to master in a matter of hours.
-        </li>
-        <li class="whitespace-nowrap mb-10 flex items-center">
-            <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-         </svg>
-            Connect with a global community of curious creatives.
-        </li>
-    </ul>
-</div>
-
-
+          <div class="md:w-1/2 p-2">
+            <ul class="list-none pl-4 text-xl md:text-2xl">
+              <li class="mb-6 md:mb-10 flex items-center">
+                <svg class="w-3.5 h-3.5 md:w-4.5 md:h-4.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0"
+                  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                Learn creative skills to achieve your personal and professional goals.
+              </li>
+              <li class="mb-6 md:mb-10 flex items-center">
+                <svg class="w-3.5 h-3.5 md:w-4.5 md:h-4.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0"
+                  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                Tune in and level up at your own pace.
+              </li>
+              <li class="mb-6 md:mb-10 flex items-center">
+                <svg class="w-3.5 h-3.5 md:w-4.5 md:h-4.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0"
+                  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                Go from dabbler to master in a matter of hours.
+              </li>
+              <li class="mb-6 md:mb-10 flex items-center">
+                <svg class="w-3.5 h-3.5 md:w-4.5 md:h-4.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0"
+                  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+                Connect with a global community of curious creatives.
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </template>
