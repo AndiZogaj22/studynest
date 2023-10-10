926e0c89c7f8bfb02996ce15740d68e8c1cc93fd
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-10-10 22:15:27 +0200

    routes fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Aboutus from './pages/Aboutus';
+import Home from './pages/Home';
+import Contactus from './pages/Contactus';
+import Courses from './Components/Courses'; // Replace './path/to/RecipeDetails' with the correct path to your RecipeDetails component
+import MyDashboard from './pages/Mydashboard';
+import Newsletter from './pages/Newsletter';
 
 function App() {
 
   return (
-    <>
-     <h1 className=''>zaza</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/contactus" element={<Contactus />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path='/mydashboard' element={<MyDashboard />}></Route>
+        <Route path='/newsletter' element={<Newsletter />}></Route>
+      </Routes>
+    </Router>
   )
 }
 
